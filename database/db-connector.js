d8742fc70349d3141c2169d6565774c5e29a7ed4
Leo Zimmer <ljzimmer2@gmail.com>
2025-03-16 21:14:57 -0700

    Final Commit. Complete

--- database/db-connector.js
+++ database/db-connector.js
@@ -7,8 +7,8 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_zimmerl',
-    password        : '0653',
+    user            : 'cs340_username',
+    password        : 'password',
     database        : 'cs340_zimmerl'
 })
 
